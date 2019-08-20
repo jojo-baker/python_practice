d0937fef9e9254fcbd6c181fae499d4e411ed337
“jojo-baker” <jo.alice.baker@gmail.com>
2019-08-20 11:01:09 +0800

    Solved row and column append

--- exercise2.py
+++ exercise2.py
@@ -11,22 +11,77 @@ five_by_five_grid = [
     ['X','0','0','X','X'],
 ]
 
-# first step is to add colum 6 and row 6
+# Count number of x's in each row and if even, append with a 0 otherwise append with an X
+for row in five_by_five_grid:
+    x_count = row.count('X')
+    if x_count % 2 == 0:
+        row.append ('0')
+    else:
+        row.append ('X')
 
-# count how many X values in row 1
-#   if x = odd then appeand X
-#   if x = even then append with 0
-# repeat for all rows
+# Count the number of x's in first value of each list, if even, append with a 0 otherwise append with an X
+# Another for loop to check item 0 in each row - define each col and then figure out how to simplify the code
 
-five_by_five_grid = [ int(items) for items in five_by_five_grid]
+sum1 = 0
+for row in five_by_five_grid:
+   xcol1 = row[0].count('X')
+   sum1 = sum1 + xcol1
+if sum1 % 2 == 0:
+        col1 = '0'
+else:
+        col1 = 'X'
 
+sum2 = 0
+for row in five_by_five_grid:
+   xcol2 = row[1].count('X')
+   sum2 = sum2 + xcol2
+if sum2 % 2 == 0:
+        col2 = '0'
+else:
+        col2 = 'X'
 
-list_1_total = sum(five_by_five_grid[0])
-print(list_1_total)
+sum3 = 0
+for row in five_by_five_grid:
+   xcol3 = row[2].count('X')
+   sum3 = sum3 + xcol3
+if sum3 % 2 == 0:
+        col3 = '0'
+else:
+        col3 = 'X'
+
+sum4 = 0
+for row in five_by_five_grid:
+   xcol4 = row[3].count('X')
+   sum4 = sum4 + xcol4
+if sum4 % 2 == 0:
+        col4 = '0'
+else:
+        col4 = 'X'
+
+sum5 = 0
+for row in five_by_five_grid:
+   xcol5 = row[4].count('X')
+   sum5 = sum5 + xcol5
+if sum5 % 2 == 0:
+        col5 = '0'
+else:
+        col5 = 'X'
+
+sum6 = 0
+for row in five_by_five_grid:
+   xcol6 = row[5].count('X')
+   sum6 = sum6 + xcol6
+if sum6 % 2 == 0:
+        col6 = '0'
+else:
+        col6 = 'X'
+
+five_by_five_grid.append ([col1, col2, col3, col4, col5, col6])
 
 # output the grid to the user
-for item in five_by_five_grid:
-    print(item)
+for row in five_by_five_grid:
+    print(row)
+
 # ask the user for the coordinate of the card to flip
 # e.g. input could be: (0,2)
 coordinate = input('Enter a coordinate ')
